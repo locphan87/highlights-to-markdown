e3dbc9d379f0b4d7536d75f8f30584377bb82764
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-05-16 15:18:54 +0200

    group by books

--- parser.js
+++ parser.js
@@ -5,19 +5,36 @@ const DATE_FORMAT = 'D [escaped] MMMM [escaped] YYYY H:mm:ss';
 
 function parse(input) {
     const rawClippings = input.split(SEPARATOR).filter((clipping) => clipping != "");
+    let books = [];
 
     const processedClippings = rawClippings.map((clipping) => {
         const [ bookData, data, empty, quote ]= clipping.trim().split('\n');
 
-        return {
-            book: getBookName(bookData),
-            author: getAuthor(bookData),
-            date: getDate(data),
-            quote: quote
+        const currentBookName = getBookName(bookData);
+
+        var existingBooks = books.filter(b => b.book === currentBookName);
+
+        if (existingBooks.length > 0)
+        {
+            existingBooks[0].quotes.push({
+                quote: quote,
+                date: getDate(data),
+            });
+        }
+        else
+        {
+            books.push({
+                book: getBookName(bookData),
+                author: getAuthor(bookData),
+                quotes: [{
+                    quote: quote,
+                    date: getDate(data),
+                }]
+            });
         }
     });
 
-    return processedClippings;
+    return books;
 }
 
 function getDate(data) {
--- parser.test.js
+++ parser.test.js
@@ -9,17 +9,29 @@ describe('MyClippings to Mardown parser should', () => {
   });
 
 test('split clippings', () => {
-    expect(parse(myClippings).length).toBe(76);
+    const books = parse(myClippings);
+
+    let totalClippings = 0;
+    books.map(b => totalClippings += b.quotes.length);
+    expect(totalClippings).toBe(76);
   });
 
 test('map every quote', () => {
     const result = parse(myClippings);
 
-    expect(result[5]).toStrictEqual({
+    expect(result[1]).toStrictEqual({
       book: 'Scrum And Xp From The Trenches',
       author: 'Henrik Kniberg',
-      date: '21/02/2015',
-      quote: 'Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.'
+      quotes: [{
+        date: '21/02/2015',
+        quote: 'Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.'
+      }]
     });
+  });
+
+  test('group by books', () => {
+    const result = parse(myClippings);
+
+    expect(result.length).toBe(20);
   });
 });
