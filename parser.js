91ed24dda10b2dbba9669f552cdc8c3371485fca
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-05-16 14:51:51 +0200

    get author and date

--- parser.js
+++ parser.js
@@ -1,19 +1,40 @@
-const SEPARATOR = "==========";
+const moment = require('moment');
+
+const SEPARATOR = '==========';
+const DATE_FORMAT = 'D [escaped] MMMM [escaped] YYYY H:mm:ss';
 
 function parse(input) {
     const rawQuotes = input.split(SEPARATOR).filter((clipping) => clipping != "");
 
     const processedQuotes = rawQuotes.map((clipping) => {
-        const [ book, data, empty, quote ]= clipping.trim().split('\n');
+        const [ bookData, data, empty, quote ]= clipping.trim().split('\n');
 
         return {
-            book: book.trim(),
+            book: getBookName(bookData),
+            author: getAuthor(bookData),
+            date: getDate(data),
             quote: quote
         }
     });
 
     return processedQuotes;
+}
 
+function getDate(data) {
+    if (data) {
+        const spanishDate = data.substring(data.lastIndexOf(",") + 2, data.lenght).trim();
+        return moment(spanishDate, DATE_FORMAT, 'es').calendar();
+    }
+
+    return "";
+}
+
+function getBookName(bookData) {
+    return bookData.substring(0, bookData.lastIndexOf("(")).trim();
+}
+
+function getAuthor(bookData) {
+    return bookData.substring(bookData.lastIndexOf("(") + 1, bookData.lastIndexOf(")"));
 }
 
 module.exports = parse;
