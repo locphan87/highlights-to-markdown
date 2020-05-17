1a8b71072a6df470cbd0395839161925270a8dc1
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-05-17 10:58:44 +0200

    refactor test

--- markdownBuilder.test.js
+++ markdownBuilder.test.js
@@ -2,8 +2,16 @@
 const fs = require('fs');
 const markdownBuilder = require('./markdownBuilder');
 
+const EXPECTED_PATH_1 = '2015-02-21-scrum-and-xp-from-the-trenches.md'
+const EXPECTED_PATH_2 = '2000-01-01-another-book.md'
+
 describe('markdownBuilder should', () => {
-  test('create a md file with correct name', () => {
+    afterAll(() => {
+        fs.unlinkSync(EXPECTED_PATH_1)
+        fs.unlinkSync(EXPECTED_PATH_2)
+      });
+
+  test('create a md file with correct name for each book', () => {
     const books = [{
         book: 'Scrum And Xp From The Trenches',
         author: 'Henrik Kniberg',
@@ -12,11 +20,20 @@ describe('markdownBuilder should', () => {
           date: '2015-02-21',
           quote: 'Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.'
         }]
+    },
+        {
+            book: 'another book',
+            author: 'Henrik Kniberg',
+            date: '2000-01-01',
+            quotes: [{
+              date: '2015-02-21',
+              quote: 'dsdsd'
+            }]
       }];
 
     markdownBuilder(books);
 
-    var markdownBookFile = fs.readFileSync('2015-02-21-scrum-and-xp-from-the-trenches.md', 'utf8');
-    expect(markdownBookFile).toStrictEqual('');
+    expect(fs.existsSync(EXPECTED_PATH_1, 'utf8')).toBe(true);
+    expect(fs.existsSync(EXPECTED_PATH_2, 'utf8')).toBe(true);
   });
 });
