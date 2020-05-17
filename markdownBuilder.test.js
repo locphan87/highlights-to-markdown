677842251175a35d75e97bd2a590942b71baef6e
Juan Pallarès Garbí <juan.pallaresgarbi@vistaprint.com>
2020-05-17 11:55:39 +0200

    improve tests

--- markdownBuilder.test.js
+++ markdownBuilder.test.js
@@ -9,14 +9,20 @@ const books = [{
         title: 'Scrum And Xp From The Trenches',
         author: 'Henrik Kniberg',
         date: '2015-02-21',
-        quotes: [{
-            date: '2015-02-21',
-            quote: 'Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.'
-        }]
+        quotes: [
+            {
+                date: '2015-02-21',
+                quote: 'Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.'
+            },
+            {
+                date: '2015-02-19',
+                quote: 'test'
+            }
+        ]
     },
     {
         title: 'another book',
-        author: 'Henrik Kniberg',
+        author: 'ahoter author',
         date: '2000-01-01',
         quotes: [{
           date: '2015-02-21',
@@ -45,7 +51,20 @@ describe('markdownBuilder should', () => {
           expect(fs.readFileSync(EXPECTED_PATH_1, 'utf8')).toStrictEqual(
 `---
 title: Scrum And Xp From The Trenches
-author: Henrik Kniberg
----`);
+bookauthor: Henrik Kniberg
+date: 2015-02-21
+quotes:
+  - date: 2015-02-21
+    quote: Pair programming does improve code quality. Pair programming does improve team focus (for example when the guy behind you says “hey is that stuff really necessary for this sprint?”). Surprisingly many developers that are strongly against pair programming actually haven’t tried it, and quickly learn to like it once they do try it. Pair programming is exhaustive and should not be done all day. Shifting pairs frequently is good. Pair programming does improve knowledge spread within the group. Surprisingly fast too. Some people just aren’t comfortable with pair programming. Don’t throw out an excellent programmer just because he isn’t comfortable with pair programming. Code review is an OK alternative to pair programming. The “navigator” (the guy not using the keyboard) should have a computer of his own as well. Not for development, but for doing little spikes when necessary, browsing documentation when the “driver” (the guy at the keyboard) gets stuck, etc. Don’t force pair programming upon people. Encourage people and provide the right tools but let them experiment with it at their own pace.
+  - date: 2015-02-19
+    quote: test
+---
+## *{{page.bookauthor}}*
+
+{% for quote in page.quotes %}
+#### {{ quote.date | date: '%B %d, %Y' }}
+{{ quote.quote }}
+{% endfor %}
+`);
       });
 });
--- templates.js
+++ templates.js
@@ -1,4 +1,18 @@
 exports.bookTemplate = `---
 title: {{title}}
-author: {{author}}
----`
+bookauthor: {{author}}
+date: {{date}}
+quotes:
+{{#quotes}}
+  - date: {{date}}
+    quote: {{quote}}
+{{/quotes}}
+---
+{{=<% %>=}}
+## *{{page.bookauthor}}*
+
+{% for quote in page.quotes %}
+#### {{ quote.date | date: '%B %d, %Y' }}
+{{ quote.quote }}
+{% endfor %}
+<%={{ }}=%>`
